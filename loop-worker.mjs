fd133a029b19787114b6d49ed560b1d29ba1d29a
Dave LaPalomento <dlapalomento@gmail.com>
2021-08-27 17:38:53 -0400

    Touch less requests Abort early if the request is not a GET. Add some error handling.

--- loop-worker.mjs
+++ loop-worker.mjs
@@ -5,31 +5,38 @@ import HLSLoop from './src/hls-loop.mjs'
 const loops = new Map()
 
 self.addEventListener('fetch', (event) => {
-  const request = new Request(event.request, {
-    mode: 'cors',
-    credentials: 'same-origin'
-  })
+  const request = event.request
+  if (request.method !== 'GET') {
+    // only process GET requests
+    return
+  }
+
   event.respondWith(fetch(request).then(async (response) => {
     const contentType = response.headers.get('Content-Type')
 
-    if (/application\/vnd\.apple\.mpegurl/i.test(contentType)) {
-      let loop = loops.get(request.url)
-
-      if (!loop) {
-        const m3u8 = await response.text()
-        loop = new HLSLoop(m3u8)
-        loops.set(request.url, loop)
-      }
-
-      return new Response(loop.render(), {
-        status: 200,
-        headers: {
-          ...response.headers,
-          'Cache-Control': 'max-age=0'
-        }
-      })
+    if (!/application\/vnd\.apple\.mpegurl/i.test(contentType)) {
+      // return unmodified responses for non-playlist requests
+      return response
     }
 
-    return response
+    let loop = loops.get(request.url)
+
+    if (!loop) {
+      // create and cache a loop for every new playlist URL
+      // encountered
+      const m3u8 = await response.text()
+      loop = new HLSLoop(m3u8)
+      loops.set(request.url, loop)
+    }
+
+    return new Response(loop.render(), {
+      status: 200,
+      headers: {
+        ...response.headers,
+        'Cache-Control': 'max-age=0'
+      }
+    })
+  }).catch((error) => {
+    console.error('Looper fetch error', request.method, request.url, error)
   }))
 })
